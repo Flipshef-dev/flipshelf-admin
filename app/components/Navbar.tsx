af5d7e9dab842275f319c3b3e081cef5756e6ac4
maulik6845 <maulikdhakal2063@gmail.com>
2025-06-23 09:47:16 +0545

    feat: update Navbar component with FontAwesome icons and adjust global font settings

--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -1,13 +1,15 @@
 "use client"
 import Link from 'next/link';
 import { useState } from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faHouse } from '@fortawesome/free-solid-svg-icons';
 
  export const Navbar = () => {
  const [isOpen, setIsOpen] = useState(false);  
   return (
   <nav className='bg-gray-800 text-white p-4 sm:p-6 md:justify-between md:items-center'>  
     <div className = "container mx-auto flex justify-between items-center">
-    <a href=""className='text-xl font-bold'> Logo </a>
+    <a href=""className='text-xl font-bold cursor-pointer'> <FontAwesomeIcon icon={faHouse} /> </a>
 
     <div className='hidden md:flex'>
       <Link href="/" className='mx-2 hover:text-gray-400'> Home </Link>
